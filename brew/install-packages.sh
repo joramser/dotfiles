246ce6d8d9831f83cb42c2e2dfea09012904295d
Jorge Ramírez Serrato <jramirezserrato@gmail.com>
2025-08-08 22:53:24 +0300

    Update brew packages list

--- brew/install-packages.sh
+++ brew/install-packages.sh
@@ -1,4 +1,6 @@
-brew install bun
+echo "Installing brew packages..."
+
+brew install oven-sh/bun/bun
 brew install nvm
 brew install zsh-autosuggestions
 brew install zsh-fast-syntax-highlighting
@@ -9,8 +11,14 @@ brew install starship
 brew install bat
 brew install fzf
 brew install eza
+brew install gh
 
 # Casks
 
 brew install visual-studio-code
 brew install ghostty
+brew install raycast
+brew install --cask nikitabobko/tap/aerospace
+brew install appcleaner
+
+brew install font-jetbrains-mono-nerd-font
